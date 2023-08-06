27d66be726fd59128ef0041e29356bdecde8c597
Stanley SERVICAL <32981008+stanlee974@users.noreply.github.com>
2023-08-07 01:43:40 +0200

    fix: duplication of customConverter until new release of cucumber-report-formatter

--- packages/runner-commons/src/index.ts
+++ packages/runner-commons/src/index.ts
@@ -3,6 +3,5 @@ export * from "./step-definition-generator/generate-step-definitions";
 export * from "./step-definition-generator/generate-step-definitions-documentation";
 export * from "./runner/step-definitions/_constant";
 export * from "./runner/step-definitions/_i-context";
-export * from "./runner/formatter/uuv-custom-formatter";
 import key from "./assets/i18n/template.json";
 export { key };
