d4176ad2ab9ecef7bb6254e9e14afe6a28aa85af
Stanley SERVICAL <32981008+stanlee974@users.noreply.github.com>
2024-01-23 22:28:01 +0100

    feat(a11y): add control of frame and empty title, #432

--- packages/a11y/tests/checker/2-frame/frame-checker.spec.ts
+++ packages/a11y/tests/checker/2-frame/frame-checker.spec.ts
@@ -13,10 +13,6 @@ describe("2-frame", () => {
     page = context.page;
   }
 
-  beforeEach(async () => {
-      await initA11yOnPage("frame.html");
-  });
-
   afterEach(async () => {
     await browser?.close();
   });
@@ -33,16 +29,16 @@ describe("2-frame", () => {
     }, url, enabledRules);
   }
 
-  it("2.1 should return results", async () => {
-
+  it("2.1 should return results for iframe", async () => {
+    await initA11yOnPage("iframe.html");
     const { result, summary } = await validateA11y(page.url(), ["2.1"]);
     expect(result.status).toEqual(A11yResultStatus.ERROR);
     const [_2_1_1, _2_2_1] = result.ruleResults;
     checkTest(_2_1_1, "2.1.1",
       A11yResultStatus.ERROR,
      [
+       "iframe[data-testid=iframe-with-empty-title]",
        "iframe[data-testid=iframe-without-title]",
-       // "frame[data-testid=frame-without-title]"
       ]
     );
     expect(summary).toEqual({
@@ -60,7 +56,8 @@ describe("2-frame", () => {
     });
   });
 
-  it("2.2 should return results", async () => {
+  it("2.2 should return results for iframe", async () => {
+    await initA11yOnPage("iframe.html");
     const { result, summary } = await validateA11y(page.url(), ["2.2"]);
     expect(result.status).toEqual(A11yResultStatus.MANUAL);
     const [_2_2_1] = result.ruleResults;
@@ -69,7 +66,6 @@ describe("2-frame", () => {
         [],
         [
           "iframe[data-testid=iframe-with-title]",
-          // "frame[data-testid=frame-with-title]"
         ]
     );
     expect(summary).toEqual({
@@ -87,4 +83,57 @@ describe("2-frame", () => {
     });
   });
 
+  it("2.1 should return results for frame", async () => {
+    await initA11yOnPage("frame.html");
+    const { result, summary } = await validateA11y(page.url(), ["2.1"]);
+    expect(result.status).toEqual(A11yResultStatus.ERROR);
+    const [_2_1_1, _2_2_1] = result.ruleResults;
+    checkTest(_2_1_1, "2.1.1",
+     A11yResultStatus.ERROR,
+     [
+       "frame[data-testid=frame-with-empty-title]",
+       "frame[data-testid=frame-without-title]",
+     ]
+    );
+    expect(summary).toEqual({
+      status: A11yResultStatus.ERROR,
+      criteria: {
+        "2.1": {
+          status: A11yResultStatus.ERROR,
+          tests: expect.objectContaining({
+            "2.1.1": {
+              status: A11yResultStatus.ERROR
+            },
+          })
+        }
+      }
+    });
+  });
+
+  it("2.2 should return results for frame", async () => {
+    await initA11yOnPage("frame.html");
+    const { result, summary } = await validateA11y(page.url(), ["2.2"]);
+    expect(result.status).toEqual(A11yResultStatus.MANUAL);
+    const [_2_2_1] = result.ruleResults;
+    checkTest(_2_2_1, "2.2.1",
+     A11yResultStatus.MANUAL,
+     [],
+     [
+       "frame[data-testid=frame-with-title]"
+     ]
+    );
+    expect(summary).toEqual({
+      status: A11yResultStatus.MANUAL,
+      criteria: {
+        "2.2": {
+          status: A11yResultStatus.MANUAL,
+          tests: expect.objectContaining({
+            "2.2.1": {
+              status: A11yResultStatus.MANUAL
+            },
+          })
+        }
+      }
+    });
+  });
 });
