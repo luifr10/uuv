PCDevSecOps/Orange-OpenSource-uuv

feat(a11y): add rgaa topic 6, #457

packages/a11y/src/lib/query/compliant-attributes/attribut-specification.ts
@@ -1,6 +1,7 @@
 import _ from "lodash";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const $ = require("jquery/dist/jquery.min");
+import { computeAccessibleName } from "dom-accessibility-api";
 
 export interface IAttributeSpecification {
   isSatisfiedBy(element: HTMLElement, attributeName: string): boolean;
@@ -63,5 +64,29 @@ export class EqualsAttributeSpecification implements IAttributeSpecification {
       return false;
     }
     return this.expectedValueList.includes(attributeValue);
+  }
+}
+
+export class AccessibleNameNotContainsVisibleTextSpecification implements IAttributeSpecification {
+  isSatisfiedBy(element: HTMLElement, attributeName: string): boolean {
+    const visibleText = element.textContent;
+    if (_.isNull(visibleText) || _.isEmpty(visibleText)) {
+      return false;
+    }
+    const accessibleName = computeAccessibleName(element);
+    console.log("visibleText", visibleText, "accessibleName", accessibleName);
+    return accessibleName?.toLowerCase().indexOf(visibleText.toLowerCase()) === -1;
+  }
+}
+
+export class EmptyInnerTextSpecification implements IAttributeSpecification {
+  isSatisfiedBy(element: HTMLElement, attributeName: string): boolean {
+    return _.isEmpty(element.textContent);
+  }
+}
+
+export class NotEmptyInnerTextSpecification implements IAttributeSpecification {
+  isSatisfiedBy(element: HTMLElement, attributeName: string): boolean {
+    return !_.isEmpty(element.textContent);
   }
 }

luifr10/uuv

packages/a11y/src/lib/query/compliant-attributes/attribut-specification.ts
@@ -1,6 +1,7 @@
 import _ from "lodash";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const $ = require("jquery/dist/jquery.min");
+import { computeAccessibleName } from "dom-accessibility-api";
 
 export interface IAttributeSpecification {
   isSatisfiedBy(element: HTMLElement, attributeName: string): boolean;
@@ -63,5 +64,29 @@ export class EqualsAttributeSpecification implements IAttributeSpecification {
       return false;
     }
     return this.expectedValueList.includes(attributeValue);
+  }
+}
+
+export class AccessibleNameNotContainsVisibleTextSpecification implements IAttributeSpecification {
+  isSatisfiedBy(element: HTMLElement, attributeName: string): boolean {
+    const visibleText = element.textContent;
+    if (_.isNull(visibleText) || _.isEmpty(visibleText)) {
+      return false;
+    }
+    const accessibleName = computeAccessibleName(element);
+    console.debug("visibleText", visibleText, "accessibleName", accessibleName);
+    return accessibleName?.toLowerCase().indexOf(visibleText.toLowerCase()) === -1;
+  }
+}
+
+export class EmptyInnerTextSpecification implements IAttributeSpecification {
+  isSatisfiedBy(element: HTMLElement, attributeName: string): boolean {
+    return _.isEmpty(element.textContent);
+  }
+}
+
+export class NotEmptyInnerTextSpecification implements IAttributeSpecification {
+  isSatisfiedBy(element: HTMLElement, attributeName: string): boolean {
+    return !_.isEmpty(element.textContent);
   }
 }